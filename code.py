e5d0f5367fdd53225aa74c8858bc9f8421145131
sacral1 <73421109+sacral1@users.noreply.github.com>
2020-11-01 16:17:11 +0600

    Update code.py

--- code.py
+++ code.py
@@ -11,22 +11,24 @@ description = []
 release_date = []
 i = 0
 
+
 def request():
     global titles
     global rate
     global description
     global release_date
 
-    api_key = {"api_key": "c714df39f180cce5586ae9609569bb08"}
+    api_key = {"api_key":
+               "c714df39f180cce5586ae9609569bb08"}
 
-    r = requests.get('https://api.themoviedb.org/3/trending/movie/day', params=api_key)
+    r = requests.get('https://api.themoviedb.org/3/trending/movie/day',
+                     params=api_key)
 
     resp = r.text
 
     dict = json.loads(resp)
 
     res = dict['results']
-
 
     for i in range(len(res)):
 
@@ -41,19 +43,12 @@ def request():
         release_date.append(release)
 
 
-
-
-
-
-
 @bot.message_handler(commands=["start"])
 def start(m):
     keyboard = types.ReplyKeyboardMarkup(True)
     keyboard.row('Next ->')
-    bot.send_message(m.chat.id, "Нажми на кнопку, чтобы получить фильм", reply_markup=keyboard)
-
-
-
+    bot.send_message(m.chat.id, "Нажми на кнопку, чтобы получить фильм"
+                     reply_markup=keyboard)
 
 
 @bot.message_handler(content_types=['text'])
@@ -63,17 +58,14 @@ def suggest(m):
     if m.text == 'Next ->':
         if i < 19:
             bot.send_message(m.chat.id, f"Title: {titles[i]}\n"
-                                        f"Rate: {rate[i]}\n"
-                                        f"Release date: {release_date[i]}\n"
-                                        f"Plot twist: {description[i]}")
+                             f"Rate: {rate[i]}\n"
+                             f"Release date: {release_date[i]}\n"
+                             f"Plot twist: {description[i]}")
             i += 1
         else:
-            bot.send_message(m.chat.id, "на сегодня все введите команду /start")
-
+            bot.send_message(m.chat.id,
+                             "на сегодня все введите команду /start")
 
     else:
         bot.send_message(m.chat.id, "press on the button")
-
-
-
 bot.polling()
